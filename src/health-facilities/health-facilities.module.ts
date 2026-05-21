1027ce1f58865f606e1bb4afafaeea5566b34474
bsc-inf-12-20 <bsc-inf-12-20@unima.ac.mw>
2026-05-21 03:18:02 +0200

    Fix: Add ActivityLogModule to HealthFacilitiesModule imports

--- src/health-facilities/health-facilities.module.ts
+++ src/health-facilities/health-facilities.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { HealthFacility } from './entities/health-facility.entity';
 import { HealthFacilitiesService } from './health-facilities.service';
 import { HealthFacilitiesController } from './health-facilities.controller';
+import { ActivityLogModule } from '../activity-log/activity-log.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([HealthFacility])],
+  imports: [TypeOrmModule.forFeature([HealthFacility]), ActivityLogModule],
   providers: [HealthFacilitiesService],
   controllers: [HealthFacilitiesController],
   exports: [HealthFacilitiesService],
